bf9475b799e3fa43c5615f964f545eaded84f623
Mario Kirven <mariokirven.mk@gmail.com>
2024-02-24 01:26:27 -0500

    Snap item to previous position on drag end

--- lib/src/components/item.dart
+++ lib/src/components/item.dart
@@ -3,10 +3,12 @@ import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
 import 'package:sort_it_out/src/sort_it_out.dart';
 
+import '../../config.dart';
+
 class Item extends CircleComponent with DragCallbacks, HasGameReference<SortItOut> {
   Item({
     required this.currentVelocity,
-    this.itemSize = 20,
+    this.itemSize = gameHeight * 0.035,
     required super.position,
   }) : super(
             radius: itemSize,
@@ -16,8 +18,8 @@ class Item extends CircleComponent with DragCallbacks, HasGameReference<SortItOu
               ..style = PaintingStyle.fill);
 
   Vector2 initialVelocity = Vector2.zero();
-  Vector2 currentVelocity ;
-
+  Vector2 currentVelocity;
+  late Vector2 positionWhenDragged = Vector2.zero();
   final double itemSize;
 
   @override
@@ -28,23 +30,30 @@ class Item extends CircleComponent with DragCallbacks, HasGameReference<SortItOu
   }
 
   @override
-  void onDragStart(DragStartEvent event) {
+  void onDragStart(
+    DragStartEvent event,
+  ) {
     // TODO: implement onDragStart
     super.onDragStart(event);
+    positionWhenDragged = Vector2.copy(position);
+    print('Previous Position: $positionWhenDragged');
     initialVelocity = currentVelocity;
     currentVelocity = Vector2(0, 0);
   }
 
   @override
   void onDragEnd(DragEndEvent event) {
-    // TODO: implement onDragEnd
-    super.onDragEnd(event);
     currentVelocity = initialVelocity;
+    position = positionWhenDragged;
+    super.onDragEnd(event);
   }
 
   @override
   void update(double dt) {
     super.update(dt);
     position += currentVelocity * dt;
+    print('Initial Position Value $positionWhenDragged');
   }
 }
+
+
